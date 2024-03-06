735b0d861bfd60183bc74151038b481238751401
GwanSon <id147369@naver.com>
2024-03-06 17:13:45 +0900

    Initial commit

---
+++ Nado/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  Nado
+//
+//  Created by 심관혁 on 3/6/24.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Nado
+ Storyboard를 이용한 ToDoList 어플리케이션
