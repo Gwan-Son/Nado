ab261e547d92066205575d740acd83e58c864e7a
GwanSon <id147369@naver.com>
2024-03-06 22:49:08 +0900

    update 24.03.06
    ToDoList를 TableView에 추가할 수 있도록 수정하였습니다.

--- Nado/AddViewController.swift
+++ Nado/AddViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
+protocol AddViewControllerDelegate: AnyObject {
+    func saveToDo(_ item: ToDo)
+}
+
 class AddViewController: UIViewController {
 
     @IBOutlet weak var outView: UIView!
     @IBOutlet weak var addTextField: UITextField!
+    @IBOutlet weak var inView: UIView!
+    
+    weak var delegate: AddViewControllerDelegate?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // 유저의 키보드의 높이에 따라 프레임 조절
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        
+        inView.clipsToBounds = true
+        inView.layer.cornerRadius = 15
+        inView.layer.maskedCorners = CACornerMask(arrayLiteral: .layerMinXMinYCorner, .layerMaxXMinYCorner)
+        
+        // 바깥뷰의 투명도
         outView.backgroundColor = UIColor.black.withAlphaComponent(0.5)
         
         // 터치 제스쳐
@@ -25,8 +42,38 @@ class AddViewController: UIViewController {
         addTextField.becomeFirstResponder()
     }
     
+    // 입력 완료 버튼을 눌렀을 때
+    @IBAction func doneButtonTapped(_ sender: Any) {
+        let title = addTextField.text ?? ""
+        let done = false
+        let date = Date()
+        
+        let newItem = ToDo(title: title, done: done, date: date)
+        
+        delegate?.saveToDo(newItem)
+        
+        dismiss(animated: true, completion: nil)
+    }
+    // 유저의 키보드가 나타날 때
+    @objc func keyboardWillShow(notification: Notification) {
+        if let keyboardFrame = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            // 키보드가 나타날 때 뷰의 위치를 조절
+            let keyboardHeight = keyboardFrame.height
+            let newYPosition = view.frame.height - keyboardHeight - inView.frame.height
+            
+            UIView.animate(withDuration: 0.3) {
+                self.inView.frame.origin.y = newYPosition
+            }
+        }
+    }
+    
     // 터치 제스쳐를 통해 Modal을 닫음
     @objc func handleTap(sender: UITapGestureRecognizer) {
         self.dismiss(animated: true)
+    }
+    
+    // 키보드 동작 감시 종료
+    deinit {
+            NotificationCenter.default.removeObserver(self)
     }
 }
--- Nado/MainViewController.swift
+++ Nado/MainViewController.swift
@@ -7,14 +7,14 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
-
+class MainViewController: UIViewController, AddViewControllerDelegate {
     
     @IBOutlet weak var nothingPage: UIView!
     @IBOutlet weak var todoTableView: UITableView!
-    
     @IBOutlet weak var explainLabel: UILabel!
     
+    // TodoList 배열 생성
+    var todoList: [ToDo] = []
     
     // ToDo-List가 존재하는지
     var isTodoExist: Bool = false
@@ -25,6 +25,10 @@ class MainViewController: UIViewController {
         explainLabel.font = UIFont(name: "NotoSansKR-Regular", size: 18)
         
         presentTodo(isTodoExist)
+        
+        self.todoTableView.dataSource = self
+        self.todoTableView.delegate = self
+//        self.loadToDoList() // 유저디폴트에 저장된 목록을 불러오는 함수
     }
 
     // ToDo-List가 존재하지 않을 때
@@ -35,11 +39,73 @@ class MainViewController: UIViewController {
             todoTableView.isHidden = true
         }
     }
+    
+    // ToDoList 저장 함수
+    func saveToDo(_ item: ToDo) {
+        todoList.append(item)
+        if isTodoExist == false { 
+            isTodoExist = true
+            presentTodo(isTodoExist)
+        }
+        print("\(todoList)")
+        todoTableView.reloadData()
+//        let data = self.todoList.map{
+//            [
+//                "title": $0.title,
+//                "done": $0.done,
+//                "data": $0.date
+//            ]
+//        }
+//        let userDefaults = UserDefaults.standard
+//        userDefaults.set(data, forKey: "todoList")
+    }
+    
     @IBAction func addButtonTapped(_ sender: Any) {
         let storyboard = UIStoryboard(name: "Add", bundle: nil)
         let vc = storyboard.instantiateViewController(identifier: "AddViewController") as! AddViewController
+        vc.delegate = self
         vc.modalPresentationStyle = .overCurrentContext
         vc.modalTransitionStyle = .crossDissolve
         present(vc, animated: true)
+    }
+    
+    func loadToDoList() {
+        let userDefaults = UserDefaults.standard
+        guard let data = userDefaults.object(forKey: "todoList") as? [[String: Any]] else { return }
+        self.todoList = data.compactMap{
+            guard let title = $0["title"] as? String else { return nil }
+            guard let done = $0["done"] as? Bool else { return nil }
+            guard let date = $0["date"] as? Date else { return nil }
+            return ToDo(title: title, done: done, date: date)
+        }
+    }
+}
+
+extension MainViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.todoList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoCell", for: indexPath)
+        let todo = self.todoList[indexPath.row]
+        cell.textLabel?.text = todo.title
+        
+        if todo.done {
+            cell.imageView?.image = UIImage(systemName: "checkmark.circle.fill")
+        } else {
+            cell.imageView?.image = UIImage(systemName: "circle")
+        }
+        
+        return cell
+    }
+}
+
+extension MainViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        var todo = self.todoList[indexPath.row]
+        todo.done = !todo.done
+        self.todoList[indexPath.row] = todo
+        self.todoTableView.reloadRows(at: [indexPath], with: .automatic)
     }
 }
